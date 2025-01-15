5f951a6147da3f32e4753f5a8f3dacdb190fba1a
Arturo Carrasco González <arturocarrascogonzalez@hotmail.com>
2025-01-15 08:47:22 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 //app.js is the express app that will be used to serve the API. 
 
 const express = require('express'); // Import express
+const cors = require('cors'); // Import cors
 
 const app = express(); // Create an express app
-
 app.use(express.json()); // Enable the app to parse JSON payloads
+app.use(cors()); // Enable the app to use cors
 
-app.use('/api', require('./routes/apiRoutes')); // Use the apiRoutes
+/// Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 module.exports = app; // Export the app
