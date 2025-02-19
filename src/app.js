8afdba9c2f70806cfe3e72034e1f5cda972e424c
Arturo Carrasco González <acarrasco@leasba.com>
2025-02-19 10:25:06 +0100

    Actualización

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@
 
 const express = require('express'); // Import express
 const cors = require('cors'); // Import cors
+const apiRoutes = require('./routes/apiRoutes'); // Importa las rutas de la API
 
 const app = express(); // Create an express app
 app.use(express.json()); // Enable the app to parse JSON payloads
 app.use(cors()); // Enable the app to use cors
+
+// Define un prefijo para todas las rutas de la API
+app.use('/api', apiRoutes);
 
 /// Error handler
 app.use((err, req, res, next) => {
